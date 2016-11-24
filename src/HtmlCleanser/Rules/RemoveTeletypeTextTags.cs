d366e12110af6e3937d407d05fe937e1f6793471
Mateusz Woźniak <crow.mw@gmail.com>
2016-11-24 09:17:58 +0100

    Add rule: RemoveTeletypeTextTags The <tt> tag is not supported in HTML5. Telerik HtmlDataProvider not recognize it and dont show inside text at all.

---
+++ src/HtmlCleanser/Rules/RemoveTeletypeTextTags.cs
@@ -0,0 +1,21 @@
+﻿using HtmlAgilityPack;
+
+namespace HtmlCleanser.Rules
+{
+    public class RemoveTeletypeTextTags : IHtmlCleanserRule
+    {
+        public void Perform(HtmlDocument doc)
+        {
+            var nodes = doc.DocumentNode.SelectNodes("//tt");
+            if (nodes == null) return;
+            foreach (var node in nodes)
+            {
+                foreach (HtmlNode child in node.ChildNodes)
+                {
+                    node.ParentNode.InsertBefore(child, node);
+                    node.Remove();
+                }
+            }
+        }
+    }
+}
